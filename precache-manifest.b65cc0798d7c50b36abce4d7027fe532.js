fe0e7f831b6e65845a6732226e7cc782cf2bf3e4
Felix G. Rios <felixg@sherplay.com>
2021-06-14 12:15:47 +0200

    Updates

---
+++ precache-manifest.b65cc0798d7c50b36abce4d7027fe532.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870b465c3ab54ebbea63e2a72cb82f97",
+    "url": "/phosphor/index.html"
+  },
+  {
+    "revision": "8b460934fe2d0526393e",
+    "url": "/phosphor/static/css/main.52a3b524.chunk.css"
+  },
+  {
+    "revision": "f1c151f88082dd4e637d",
+    "url": "/phosphor/static/js/2.8b28d920.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/phosphor/static/js/2.8b28d920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b460934fe2d0526393e",
+    "url": "/phosphor/static/js/main.b25bdee6.chunk.js"
+  },
+  {
+    "revision": "5c62f6ff5721c579ea5b",
+    "url": "/phosphor/static/js/runtime-main.980b6d63.js"
+  },
+  {
+    "revision": "063c1c7da40a8ea33a3457c3737c2e66",
+    "url": "/phosphor/static/media/vga.063c1c7d.woff"
+  },
+  {
+    "revision": "2c3e089d0847907c32ec1c9c9169e05b",
+    "url": "/phosphor/static/media/vga.2c3e089d.woff2"
+  },
+  {
+    "revision": "634410d4b532b9d0dd89fd916fbffe4e",
+    "url": "/phosphor/static/media/vga.634410d4.ttf"
+  }
+]);
